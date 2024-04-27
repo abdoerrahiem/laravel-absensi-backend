b6d86b3c930c373c057289bcb6aa35a0491fa523
Abdur Rahim <abdoerrahiem@gmail.com>
2024-04-27 18:54:54 +0700

    user management

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+
+    Route::resource('users', UserController::class);
+});
